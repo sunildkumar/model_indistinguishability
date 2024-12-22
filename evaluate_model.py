66cd06f270d827746979a85d5fecaab33e9bde90
Sunil Kumar <sdkumar@hmc.edu>
2024-12-22 06:06:06 +0000

    eval results on all models on all the data

--- evaluate_model.py
+++ evaluate_model.py
@@ -1,5 +1,6 @@
-from collections import Counter
+import random
 
+import pandas as pd
 from torch.utils.data import DataLoader
 from torchvision import transforms
 from tqdm import tqdm
@@ -12,7 +13,7 @@ from model_zoo.vit import ViTModel
 from model_zoo.vit16_lora import ViT16LoraModel
 
 
-def evaluate_model(model, batches_to_eval=None):
+def evaluate_model(model):
     model.load_model()
 
     transform = transforms.ToTensor()
@@ -22,41 +23,78 @@ def evaluate_model(model, batches_to_eval=None):
 
     predicted_labels = []
     true_labels = []
+    fnames = []
 
-    for i, batch in enumerate(tqdm(dataloader, desc="Evaluating model")):
-        if batches_to_eval is not None and i >= batches_to_eval:
-            break
+    for batch in tqdm(dataloader, desc="Evaluating model"):
         images, labels, filenames = batch
         predicted_classes = model.predict(images)
         predicted_labels.extend(predicted_classes.tolist())
         true_labels.extend(labels.tolist())
-
+        fnames.extend(filenames)
     accuracy = sum(p == t for p, t in zip(predicted_labels, true_labels)) / len(
         true_labels
     )
     print(f"Model: {model} Accuracy: {accuracy}")
 
-    # Print the distribution of the predicted labels
-    label_distribution = Counter(predicted_labels)
-    print("Predicted label distribution:", label_distribution)
-
     model.teardown_model()
+
+    return list(zip(fnames, predicted_labels, true_labels))
 
 
 if __name__ == "__main__":
     print("evaluating models")
+    random.seed(42)
 
     vit_model = ViTModel()
-    evaluate_model(vit_model)
+    vit_results = evaluate_model(vit_model)
 
     convnext_model = ConvNextModel()
-    evaluate_model(convnext_model)
+    convnext_results = evaluate_model(convnext_model)
 
     swin_model = SwinModel()
-    evaluate_model(swin_model)
+    swin_results = evaluate_model(swin_model)
 
     vit16_lora_model = ViT16LoraModel()
-    evaluate_model(vit16_lora_model)
+    vit16_lora_results = evaluate_model(vit16_lora_model)
 
     resnet_model = ResNet20Model()
-    evaluate_model(resnet_model)
+    resnet_results = evaluate_model(resnet_model)
+
+    df = pd.DataFrame(
+        columns=[
+            "filename",
+            "True label",
+            "ViT Label",
+            "ConvNext Label",
+            "Swin Label",
+            "ViT16Lora Label",
+            "ResNet Label",
+        ]
+    )
+
+    # populate the fnames and true labels
+    for fname, predicted_label, true_label in vit_results:
+        df.loc[fname, "filename"] = fname
+        df.loc[fname, "True label"] = true_label
+
+    # populate the vit labels
+    for fname, predicted_label, true_label in vit_results:
+        df.loc[fname, "ViT Label"] = predicted_label
+
+    # populate the convnext labels
+    for fname, predicted_label, true_label in convnext_results:
+        df.loc[fname, "ConvNext Label"] = predicted_label
+
+    # populate the swin labels
+    for fname, predicted_label, true_label in swin_results:
+        df.loc[fname, "Swin Label"] = predicted_label
+
+    # populate the vit16 lora labels
+    for fname, predicted_label, true_label in vit16_lora_results:
+        df.loc[fname, "ViT16Lora Label"] = predicted_label
+
+    # populate the resnet labels
+    for fname, predicted_label, true_label in resnet_results:
+        df.loc[fname, "ResNet Label"] = predicted_label
+
+    df.to_csv("model_eval_results.csv", index=False)
